2c468c12847561d7d9fc2fdbbdebef43566b8473
kelvinwieth <wiethkelvin@gmail.com>
2024-11-30 11:01:32 -0300

    rename begginer files

--- scripts/make_files_snake_case.sh
+++ scripts/make_files_snake_case.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 for file in *.md; do
-  snake_case_name=$(echo "$file" | \
-    sed -E 's/-/_/g' | \
-    sed -E 's/(.*)\.md/\L\1.md/')
+  snake_case_name=$(echo "$file" | sed -E 's/-/_/g' | tr '[:upper:]' '[:lower:]')
   mv "$file" "$snake_case_name"
 done
